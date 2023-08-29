dd1a2fdeffa42ffc64225231b15aab23ef93dc3d
Aiden Abignano <Aiden.J.Abignano@student.uts.edu.au>
2023-08-29 17:13:37 +1000

    90 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     private int i=3;
+
+    public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,6 @@ public class ConsolePrint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(this.name + "'s name:" + i++);
+        Debug.Log(this.name + ":" + ++i);
     }
 }
