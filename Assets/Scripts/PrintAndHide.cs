cdb4980bf8acb179b847b98702861aabf527e2bc
Aiden Abignano <Aiden.J.Abignano@student.uts.edu.au>
2023-08-29 17:36:53 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,17 +6,30 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i=3;
 
+    private int randInt;
+
     public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
-         
+        randInt = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(this.CompareTag("Red") && i == 100)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        if(this.CompareTag("Blue") && i == randInt)
+        {
+            rend.enabled = false;
+        }
+
         Debug.Log(this.name + ":" + ++i);
     }
 }
